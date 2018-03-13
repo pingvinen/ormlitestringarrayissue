eea7de9a2ccc34f3bd83d3978fffbf556a6784c0
Patrick Boisen <patrick@skarp.dk>
2018-03-13 13:36:06 +0100

    Add project showing issue

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ServiceStack.DataAnnotations;
+using ServiceStack.OrmLite;
+
+namespace ormlitestringarrayissue
+{
+    public class PgsqlData
+    {
+        [PrimaryKey]
+        [AutoIncrement]
+        public int Id { get; set; }
+
+        [PgSqlTextArray]
+        public List<string> ListStrings { get; set; }
+    }
+    
+    class Program
+    {
+        static string PostgreSqlDb = "Server=localhost;Port=5432;User Id=test;Password=test;Database=test;Pooling=true;MinPoolSize=0;MaxPoolSize=200";
+        
+        static void Main(string[] args)
+        {
+            var dbFactory = new OrmLiteConnectionFactory(PostgreSqlDb, PostgreSqlDialect.Provider);
+
+            using (var db = dbFactory.OpenDbConnection())
+            {
+                db.DropAndCreateTable<PgsqlData>();
+
+                db.Insert<PgsqlData>(new PgsqlData
+                {
+                    ListStrings = new [] {"hello", "there"}.ToList()
+                });
+
+                var deserialized = db.Select<PgsqlData>(x => x.Id == 1).First();
+
+                if (!deserialized.ListStrings[0].Equals("hello") || !deserialized.ListStrings[1].Equals("there"))
+                {
+                    throw new Exception("It came back wrong :/");
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# List of string deserialization
+
+To run locally:
+```
+$ docker-compose up -d
+$ dotnet run
+```
+
+If deserialization fails, an exception is thrown.
+
